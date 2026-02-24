45965ddf5706bf0b6acd259d00efc400d505cfd8
Keshan W <138182496+SekhmetDivine@users.noreply.github.com>
2026-02-24 16:35:39 -0400

    Fix base path + instructor section + deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
+  site: "https://SekhmetDivine.github.io",
+  base: isProd ? "/wireman-licence-tt" : "/",
   integrations: [tailwind()],
+  output: "static",
 });
